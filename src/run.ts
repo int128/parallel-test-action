5846653b89a3d627c3de6e5b74708dc85a7d4403
Hidetake Iwata <int128@gmail.com>
2024-10-07 19:17:12 +0900

    Show logs

--- src/run.ts
+++ src/run.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs/promises'
 import * as os from 'os'
 import { getOctokit } from './github'
 import { downloadTestReports } from './artifact'
+import { parseJunitXml } from './junitxml'
 
 type Inputs = {
   testReportBranch: string
@@ -27,5 +28,9 @@ export const run = async (inputs: Inputs): Promise<void> => {
     token: inputs.token,
   })
 
-  core.info(testReportFiles.join('\n'))
+  for (const testReportFile of testReportFiles) {
+    const xml = await fs.readFile(testReportFile)
+    const junitXml = parseJunitXml(xml)
+    core.info(`Parsed ${testReportFile}: ${JSON.stringify(junitXml, null, 2)}`)
+  }
 }
