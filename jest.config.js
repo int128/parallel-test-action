8aec556e4160708200393ddcc034fa3398725cb4
Hidetake Iwata <int128@gmail.com>
2024-11-23 13:33:54 +0900

    Use @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
   clearMocks: true,
   // https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
   reporters: [
     'default',
