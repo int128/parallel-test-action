273c809ef8adad89de1621b1983ae595ea825d83
int128 <int128@users.noreply.github.com>
2025-04-03 23:39:17 +0000

    Apply tasks/migrate-vitest
    GitHub Actions: https://github.com/int128/actions-tanpopo-bot/actions/runs/14254813765

--- tests/shard.test.ts
+++ tests/shard.test.ts
@@ -1,3 +1,7 @@
+import { test } from 'vitest'
+import { describe } from 'vitest'
+import { it } from 'vitest'
+import { expect } from 'vitest'
 import { distributeTestFilesToShards } from '../src/shard.js'
 
 describe('distributeTestFilesToShards', () => {
