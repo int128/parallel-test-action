0542a38caaf96b0164c72205dd20d290327dcef8
Hidetake Iwata <int128@gmail.com>
2025-04-28 12:40:50 +0900

    Set addFileAttribute to junit reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     clearMocks: true,
-    reporters: ['junit'],
+    reporters: [['default'], ['junit', { addFileAttribute: true }]],
     outputFile: {
       junit: './junit.xml',
     },
