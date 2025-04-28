b40c34732f9422d778dae48e1203f220b5434929
Hidetake Iwata <int128@gmail.com>
2025-04-28 11:40:03 +0900

    Enable junit reporter in vitest (#227)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     clearMocks: true,
+    reporters: ['junit'],
+    outputFile: {
+      junit: './junit.xml',
+    },
   },
 })
