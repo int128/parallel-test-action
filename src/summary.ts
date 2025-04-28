0e9b43b067241f6c48114e623e220bbc3d354f0c
Hidetake Iwata <int128@gmail.com>
2025-04-28 13:10:27 +0900

    Revise summary

--- src/summary.ts
+++ src/summary.ts
@@ -3,34 +3,14 @@ import { ShardSet } from './shard.js'
 import { TestWorkflowRun } from './artifact.js'
 
 export const writeSummary = (shardSet: ShardSet, testWorkflowRun: TestWorkflowRun | undefined) => {
-  core.summary.addHeading('Summary of parallel-test-action')
+  core.summary.addHeading('Summary of parallel-test-action', 2)
   core.summary.addRaw(
     'This action distributes the test files to the shards based on the estimated time from the test reports.',
     true,
   )
-  core.summary.addHeading('Shards', 2)
-  core.summary.addTable([
-    [
-      { data: 'ID', header: true },
-      { data: 'Test files', header: true },
-      { data: 'Estimated test cases', header: true },
-      { data: 'Estimated time (m:s)', header: true },
-    ],
-    ...shardSet.shards.map((shard) => [
-      `#${shard.id}`,
-      `${shard.testFiles.length}`,
-      `${shard.totalTestCases}`,
-      formatTimeInMinSec(shard.totalTime),
-    ]),
-    [
-      { data: 'Total', header: true },
-      { data: `${shardSet.shards.reduce((x, y) => x + y.testFiles.length, 0)}` },
-      { data: `${shardSet.shards.reduce((x, y) => x + y.totalTestCases, 0)}` },
-      { data: formatTimeInMinSec(shardSet.shards.reduce((x, y) => x + y.totalTime, 0)) },
-    ],
-  ])
 
-  core.summary.addHeading('Test files in the working directory', 2)
+  core.summary.addHeading('Input: Test files', 3)
+  core.summary.addRaw(`Found ${shardSet.workingTestFiles.length} test files in the working directory.`, true)
   core.summary.addRaw('<details>')
   core.summary.addTable([
     [
@@ -52,15 +32,38 @@ export const writeSummary = (shardSet: ShardSet, testWorkflowRun: TestWorkflowRu
   )
   core.summary.addRaw('</details>')
 
-  core.summary.addHeading('Test reports', 2)
+  core.summary.addHeading('Input: Test reports', 3)
   if (testWorkflowRun) {
     core.summary.addRaw('This action downloaded the test reports from ')
     core.summary.addLink('the last success workflow run', testWorkflowRun.url)
-    core.summary.addHeading('Files', 3)
+    core.summary.addRaw(' to estimate the time of the test files.')
+    core.summary.addHeading('Downloaded files', 4)
     core.summary.addList(testWorkflowRun.testReportFiles)
   } else {
-    core.summary.addRaw('No test reports found')
+    core.summary.addRaw('No test reports found in the last success workflow run.')
   }
+
+  core.summary.addHeading('Output: Test shards', 3)
+  core.summary.addTable([
+    [
+      { data: 'ID', header: true },
+      { data: 'Test files', header: true },
+      { data: 'Estimated test cases', header: true },
+      { data: 'Estimated time (m:s)', header: true },
+    ],
+    ...shardSet.shards.map((shard) => [
+      `#${shard.id}`,
+      `${shard.testFiles.length}`,
+      `${shard.totalTestCases}`,
+      formatTimeInMinSec(shard.totalTime),
+    ]),
+    [
+      { data: 'Total', header: true },
+      { data: `${shardSet.shards.reduce((x, y) => x + y.testFiles.length, 0)}` },
+      { data: `${shardSet.shards.reduce((x, y) => x + y.totalTestCases, 0)}` },
+      { data: formatTimeInMinSec(shardSet.shards.reduce((x, y) => x + y.totalTime, 0)) },
+    ],
+  ])
 }
 
 export const formatTimeInMinSec = (seconds: number): string => {
