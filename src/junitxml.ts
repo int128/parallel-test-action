548fc9d02cd482063510ad6a63eeb7c44a57ca57
Hidetake Iwata <int128@gmail.com>
2025-12-06 10:50:51 +0900

    Use zod for validation of junit.xml (#408)
    * Use zod for validation of junit.xml
    
    * Fix schema

--- src/junitxml.ts
+++ src/junitxml.ts
@@ -1,8 +1,8 @@
-import assert from 'node:assert'
 import * as fs from 'node:fs/promises'
 import * as path from 'node:path'
 import * as core from '@actions/core'
 import { XMLParser } from 'fast-xml-parser'
+import * as z from 'zod'
 
 type TestFile = {
   filename: string
@@ -43,7 +43,7 @@ export type TestCase = {
 }
 
 export const findTestCasesFromJunitXml = (junitXml: JunitXml): TestCase[] => {
-  const root = junitXml.testsuites?.testsuite ?? junitXml.testsuite ?? []
+  const rootTestSuites: JunitXmlTestSuite[] = junitXml.testsuites?.testsuite ?? junitXml.testsuite ?? []
 
   function* visit(testSuite: JunitXmlTestSuite): Generator<TestCase> {
     const determineTestCaseFilename = (junitXmlTestCase: JunitXmlTestCase): string => {
@@ -51,7 +51,7 @@ export const findTestCasesFromJunitXml = (junitXml: JunitXml): TestCase[] => {
         return junitXmlTestCase['@_file']
       }
       // For Mocha or Cypress, the first <testsuite> element has the filename of the root suite.
-      const mochaRootSuiteFilename = root.at(0)?.['@_file']
+      const mochaRootSuiteFilename = rootTestSuites.at(0)?.['@_file']
       if (mochaRootSuiteFilename) {
         return mochaRootSuiteFilename
       }
@@ -70,7 +70,7 @@ export const findTestCasesFromJunitXml = (junitXml: JunitXml): TestCase[] => {
   }
 
   const testCases: TestCase[] = []
-  for (const testSuite of root) {
+  for (const testSuite of rootTestSuites) {
     for (const testCase of visit(testSuite)) {
       testCases.push(testCase)
     }
@@ -91,77 +91,34 @@ const parseTestReportFilesToJunitXml = async (testReportFiles: string[]): Promis
   return junitXmls
 }
 
-type JunitXml = {
-  testsuites?: {
-    testsuite?: JunitXmlTestSuite[]
-  }
-  testsuite?: JunitXmlTestSuite[]
-}
+const JunitXmlTestCase = z.object({
+  '@_name': z.string(),
+  '@_time': z.number(),
+  '@_file': z.string().optional(),
+})
 
-function assertJunitXml(x: unknown): asserts x is JunitXml {
-  assert(typeof x === 'object', 'Root document must be an object')
-  assert(x != null, 'Root document must not be null')
+type JunitXmlTestCase = z.infer<typeof JunitXmlTestCase>
 
-  if ('testsuites' in x) {
-    assert(typeof x.testsuites === 'object', 'Element <testsuites> must be an object')
-    assert(x.testsuites != null, 'Element <testsuites> must not be null')
+const JunitXmlTestSuite = z.object({
+  get testsuite() {
+    return z.array(JunitXmlTestSuite).optional()
+  },
+  testcase: z.array(JunitXmlTestCase).optional(),
+  '@_file': z.string().optional(),
+})
 
-    if ('testsuite' in x.testsuites) {
-      assert(Array.isArray(x.testsuites.testsuite), 'Element <testsuite> must be an array')
-      for (const testsuite of x.testsuites.testsuite) {
-        assertTestSuite(testsuite)
-      }
-    }
-  }
+type JunitXmlTestSuite = z.infer<typeof JunitXmlTestSuite>
 
-  if ('testsuite' in x) {
-    assert(Array.isArray(x.testsuite), 'Element <testsuite> must be an array')
-    for (const testsuite of x.testsuite) {
-      assertTestSuite(testsuite)
-    }
-  }
-}
+const JunitXml = z.object({
+  testsuites: z
+    .object({
+      testsuite: z.array(JunitXmlTestSuite).optional(),
+    })
+    .optional(),
+  testsuite: z.array(JunitXmlTestSuite).optional(),
+})
 
-type JunitXmlTestSuite = {
-  testsuite?: JunitXmlTestSuite[]
-  testcase?: JunitXmlTestCase[]
-  '@_file'?: string
-}
-
-function assertTestSuite(x: unknown): asserts x is JunitXmlTestSuite {
-  assert(typeof x === 'object', 'Element <testsuite> must be an object')
-  assert(x != null, 'Element <testsuite> must not be null')
-  if ('testsuite' in x) {
-    assert(Array.isArray(x.testsuite), 'Element <testsuite> must be an array')
-    for (const testsuite of x.testsuite) {
-      assertTestSuite(testsuite)
-    }
-  }
-  if ('testcase' in x) {
-    assert(Array.isArray(x.testcase), 'Element <testcase> must be an array')
-    for (const testcase of x.testcase) {
-      assertTestCase(testcase)
-    }
-  }
-}
-
-type JunitXmlTestCase = {
-  '@_name': string
-  '@_time': number
-  '@_file'?: string
-}
-
-function assertTestCase(x: unknown): asserts x is JunitXmlTestCase {
-  assert(typeof x === 'object', 'Element <testcase> must be an object')
-  assert(x != null, 'Element <testcase> must not be null')
-  assert('@_name' in x, 'Element <testcase> must have "name" attribute')
-  assert(typeof x['@_name'] === 'string', 'name attribute of <testcase> must be a string')
-  assert('@_time' in x, 'Element <testcase> must have "time" attribute')
-  assert(typeof x['@_time'] === 'number', 'time attribute of <testcase> must be a number')
-  if ('@_file' in x) {
-    assert(typeof x['@_file'] === 'string', 'file attribute of <testcase> must be a string')
-  }
-}
+type JunitXml = z.infer<typeof JunitXml>
 
 export const parseJunitXml = (xml: string | Buffer): JunitXml => {
   const parser = new XMLParser({
@@ -182,7 +139,6 @@ export const parseJunitXml = (xml: string | Buffer): JunitXml => {
       return attrValue
     },
   })
-  const parsed: unknown = parser.parse(xml)
-  assertJunitXml(parsed)
-  return parsed
+  const parsed = parser.parse(xml)
+  return JunitXml.parse(parsed)
 }
